80e53937e86ea6e17ebc0eab1e460d6618a33c14
Jean-Baptiste <jbginguene@gmail.com>
2021-06-02 16:55:42 +0200

    ajax inscription en cours

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,27 +1,55 @@
 //une fois que la page est chargée on appel la fonction init
 window.onload = init;
 
-function init(){
-    let select = document.getElementById('sortie_lieu');
-    let genererButton = document.getElementById('generer_adresse')
-    let lieu_field = select.options[select.selectedIndex].text;
+function init() {
+    console.log(window.location.pathname);
 
-    genererButton.addEventListener('click',function (){
-        //pour actualiser l'élement du select
-        lieu_field = select.options[select.selectedIndex].text;
-        //On prepare un objet qui porte les infos
-        let data = {'lieu' : lieu_field};
+    // CREATION SORTIE
+    if (window.location.pathname === '/sortir.com/public/sortie/create') {
+        let select = document.getElementById('sortie_lieu');
+        let genererButton = document.getElementById('generer_adresse')
+        let lieu_field = select.options[select.selectedIndex].text;
 
-        fetch("ajax-site", {method: 'POST', body: JSON.stringify(data)})
-            //promesse : le contenu du data dans le dernier then
-            .then(function (response){
-                return response.json();
-            }).then(function (data) {
-            document.getElementById('rue').innerHTML = "Adresse : " + data.rue;
-            document.getElementById('latitude').innerHTML = "Latitude : " + data.latitude;
-            document.getElementById('longitude').innerHTML = "Longitude : " + data.longitude;
-            document.getElementById('code_postal').innerHTML = "Code postal : " + data.code_postal;
-            document.getElementById('ville').innerHTML = "Ville : " + data.ville;
+        genererButton.addEventListener('click', function () {
+            //pour actualiser l'élement du select
+            lieu_field = select.options[select.selectedIndex].text;
+            //On prepare un objet qui porte les infos
+            let data = {'lieu': lieu_field};
+
+            fetch("ajax-site", {method: 'POST', body: JSON.stringify(data)})
+                //promesse : le contenu du data dans le dernier then
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                document.getElementById('rue').innerHTML = "Adresse : " + data.rue;
+                document.getElementById('latitude').innerHTML = "Latitude : " + data.latitude;
+                document.getElementById('longitude').innerHTML = "Longitude : " + data.longitude;
+                document.getElementById('code_postal').innerHTML = "Code postal : " + data.code_postal;
+                document.getElementById('ville').innerHTML = "Ville : " + data.ville;
+            });
+        })
+    }
+
+    // INSCRIPTION SORTIE (page d'accueil)
+    if (window.location.pathname === '/sortir.com/public/') {
+        let inscriptionButtons = Array.from(document.getElementsByClassName('sortie_inscription'));
+        console.log(inscriptionButtons);
+
+        inscriptionButtons.forEach(function (elem, idx) {
+            elem.addEventListener('click', function () {
+                let data = {'sortieid': elem.dataset.sortieid, 'userid': elem.dataset.userid};
+                console.log(data);
+                console.log(data.sortieid)
+                console.log(data.userid)
+
+                fetch("ajax-sortie-inscription", {method: 'POST', body: JSON.stringify(data)})
+                    //promesse : le contenu du data dans le dernier then
+                    .then(function (response) {
+                        return response.json();
+                    }).then(function (data) {
+                        sortieidstrg = toString(data.sortieid);
+                    document.querySelectorAll("[data-sortieid='" + data.sortieid + "']").innerHTML = "X";
+                });
+            });
         });
-    })
-}
+    }}
