1b21e87f428082ae2f7abb1204d77bc76df710ea
Jean-Baptiste <jbginguene@gmail.com>
2021-06-02 10:06:58 +0200

    profile details

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Profiler\Profile;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -17,21 +16,20 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile/{id}", name="app_profile")
      */
-
     public function edit($id,
                          ParticipantRepository $participantRepository,
                          Request $request,
                          EntityManagerInterface $entityManager): Response
     {
 
-        $profile =$participantRepository->find($id);
-        if (!$profile){
-            throw $this->createNotFoundException("ce profil n'existe pas");
+        $profile = $participantRepository->find($id);
+        if (!$profile) {
+            throw $this->createNotFoundException("Ce profil n'existe pas");
         }
         $profileForm = $this->createForm(ProfileFormType::class, $profile);
         $profileForm->handleRequest($request);
 
-        if ($profileForm->isSubmitted()&& $profileForm->isValid()){
+        if ($profileForm->isSubmitted() && $profileForm->isValid()) {
             $entityManager->persist($profile);
             $entityManager->flush();
 
@@ -41,7 +39,21 @@ class ProfileController extends AbstractController
 
         return $this->render('profile/edit.html.twig', [
             'profile' => $profile,
-            'profileForm'=>$profileForm->createView()
+            'profileForm' => $profileForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/profile/detail/{id}", name="profile_detail")
+     */
+    public function detail($id, ParticipantRepository $participantRepository): Response
+    {
+        $profile = $participantRepository->find($id);
+        if (!$profile) {
+            throw $this->createNotFoundException("Désolé, ce profil n'existe pas");
+        }
+        return $this->render('profile/detail.html.twig', [
+            'profile' => $profile,
         ]);
     }
 
