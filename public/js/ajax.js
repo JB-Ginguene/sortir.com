1720c09647322fe5d8208b8410b21330fa68cb5d
Jean-Baptiste <jbginguene@gmail.com>
2021-06-03 11:26:25 +0200

    ajax homepage almost OK

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,10 +1,86 @@
 //une fois que la page est chargée on appel la fonction init
 window.onload = init;
 
+function inscriptionDesinscription() {
+    let inscriptionButtons = Array.from(document.getElementsByClassName('sortie_inscription'));
+    console.log("inscriptionButtons début : ");
+    console.log(inscriptionButtons);
+
+    let desinscriptionButtons = Array.from(document.getElementsByClassName('sortie_desinscription'));
+    console.log("desinscriptionButtons début : ");
+    console.log(desinscriptionButtons);
+
+    //INSCRIPTION
+    inscriptionButtons.forEach(function (elem, idx) {
+        elem.addEventListener('click', function () {
+            let data = {'sortieid': elem.dataset.sortieid, 'userid': elem.dataset.userid};
+
+            fetch("ajax-sortie-inscription", {method: 'POST', body: JSON.stringify(data)})
+                //promesse : le contenu du data dans le dernier then
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                console.log("yolo inscription")
+
+                let td = document.createElement('td');
+                let a = document.createElement('a');
+                a.classList.add("bg-success");
+                a.classList.add("btn");
+                a.classList.add("btn-sm");
+                a.classList.add("sortie_desinscription");
+                a.setAttribute("role", "button");
+                a.setAttribute("data-sortieId", data.sortieid);
+                a.setAttribute("data-userId", data.userid);
+                a.innerHTML = "Inscrit·e";
+                td.append(a);
+                let parent = elem.parentNode;
+                if (parent.parentNode){
+                    parent.parentNode.replaceChild(td, parent);
+                }
+                inscriptionDesinscription();
+            });
+        });
+    });
+
+    //DESINSCRIPTION :
+    desinscriptionButtons.forEach(function (elem, idx) {
+        elem.addEventListener('click', function () {
+            let data = {'sortieid': elem.dataset.sortieid, 'userid': elem.dataset.userid};
+
+            fetch("ajax-sortie-desinscription", {method: 'POST', body: JSON.stringify(data)})
+                //promesse : le contenu du data dans le dernier then
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                console.log("desinscription yolooo !!!")
+                console.log(elem);
+
+                let td = document.createElement('td');
+                let a = document.createElement('a');
+                a.classList.add("bg-warning");
+                a.classList.add("btn");
+                a.classList.add("btn-sm");
+                a.classList.add("sortie_inscription");
+                a.setAttribute("role", "button");
+                a.setAttribute("data-sortieId", data.sortieid);
+                a.setAttribute("data-userId", data.userid);
+                a.innerHTML = "S'inscrire";
+                td.append(a);
+                let parent = elem.parentNode;
+                if (parent.parentNode){
+                    parent.parentNode.replaceChild(td, parent);
+                }
+
+                inscriptionDesinscription();
+            });
+        });
+    });
+
+}
+
 function init() {
 
     let url = window.location.href.split('sortir.com/public/')[1];
-    console.log('url : ' + url)
 
     // CREATION SORTIE
     if (url.includes('create')) {
@@ -61,92 +137,7 @@ function init() {
 
 // DES/INSCRIPTION SORTIE (page d'accueil)
     if (url === '') {
-        let inscriptionButtons = Array.from(document.getElementsByClassName('sortie_inscription'));
-        console.log("inscriptionButtons début : ");
-        console.log(inscriptionButtons);
-
-        let desinscriptionButtons = Array.from(document.getElementsByClassName('sortie_desinscription'));
-        console.log("desinscriptionButtons début : ");
-        console.log(desinscriptionButtons);
-
-        //INSCRIPTION
-        inscriptionButtons.forEach(function (elem, idx) {
-            elem.addEventListener('click', function () {
-                let data = {'sortieid': elem.dataset.sortieid, 'userid': elem.dataset.userid};
-                console.log(data);
-                console.log(data.sortieid)
-                console.log(data.userid)
-
-                fetch("ajax-sortie-inscription", {method: 'POST', body: JSON.stringify(data)})
-                    //promesse : le contenu du data dans le dernier then
-                    .then(function (response) {
-                        return response.json();
-                    }).then(function (data) {
-                    console.log("yolo inscription")
-                    console.log(elem);
-
-                    let td = document.createElement('td');
-                    let a = document.createElement('a');
-                    a.classList.add("bg-success");
-                    a.classList.add("btn");
-                    a.classList.add("btn-sm");
-                    a.classList.add("sortie_desinscription");
-                    a.setAttribute("role", "button");
-                    a.setAttribute("data-sortieId", data.sortieid);
-                    a.setAttribute("data-userId", data.userid);
-                    a.innerHTML = "Inscrit·e";
-                    td.append(a);
-                    let parent = elem.parentNode;
-                    parent.parentNode.replaceChild(td, parent);
-                    inscriptionButtons = Array.from(document.getElementsByClassName('sortie_inscription'));
-                    console.log("inscriptionButtons fin inscription: ");
-                    console.log(inscriptionButtons);
-                    desinscriptionButtons = Array.from(document.getElementsByClassName('sortie_desinscription'));
-                    console.log("desinscriptionButtons fin inscription: ");
-                    console.log(desinscriptionButtons);
-                });
-            });
-        });
-
-        //DESINSCRIPTION :
-       desinscriptionButtons.forEach(function (elem, idx) {
-            elem.addEventListener('click', function () {
-                let data = {'sortieid': elem.dataset.sortieid, 'userid': elem.dataset.userid};
-                console.log(data);
-                console.log(data.sortieid)
-                console.log(data.userid)
-
-                fetch("ajax-sortie-desinscription", {method: 'POST', body: JSON.stringify(data)})
-                    //promesse : le contenu du data dans le dernier then
-                    .then(function (response) {
-                        return response.json();
-                    }).then(function (data) {
-                    console.log("desinscription yolooo !!!")
-                    console.log(elem);
-
-                    let td = document.createElement('td');
-                    let a = document.createElement('a');
-                    a.classList.add("bg-warning");
-                    a.classList.add("btn");
-                    a.classList.add("btn-sm");
-                    a.classList.add("sortie_inscription");
-                    a.setAttribute("role", "button");
-                    a.setAttribute("data-sortieId", data.sortieid);
-                    a.setAttribute("data-userId", data.userid);
-                    a.innerHTML = "S'inscrire";
-                    td.append(a);
-                    let parent = elem.parentNode;
-                    parent.parentNode.replaceChild(td, parent);
-                    inscriptionButtons = Array.from(document.getElementsByClassName('sortie_inscription'));
-                    console.log("inscriptionButtons fin desinscription: ");
-                    console.log(inscriptionButtons);
-                    desinscriptionButtons = Array.from(document.getElementsByClassName('sortie_desinscription'));
-                    console.log("desinscriptionButtons fin desinscription: ");
-                    console.log(desinscriptionButtons);
-                });
-            });
-        });
-
+        inscriptionDesinscription();
     }
 }
 
